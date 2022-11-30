3af97d1a55da5a806603e67c4270156c7aab8e59
João Lucas <introducaoteste@gmail.com>
2022-11-30 13:56:53 -0300

    Criando a tela de planos, criando script para redimencionamento da tela e criando os estilos do corpo da página

---
+++ src/js/planos-MobileDesktop.js
@@ -0,0 +1,31 @@
+let telaMobile = false
+
+if (window.innerWidth >= 800) {
+    document.querySelector(".logo").innerHTML = `
+        <a href="../../index.html">
+            <img src="../../assets/logo-desktop.png" alt="logo" />
+        </a>
+    `
+    telaMobile = false
+}
+
+window.onresize = () => {
+    if (window.innerWidth >= 800) {
+        if (telaMobile) {
+            document.querySelector(".logo").innerHTML = `
+                <a href="../../index.html">
+                    <img src="../../assets/logo-desktop.png" alt="logo" />
+                </a>
+            `
+            telaMobile = false
+        }
+    }
+    if (window.innerWidth < 800) {
+        if (!telaMobile) {
+            document.querySelector(".logo").innerHTML = `
+                <a href="../../index.html">Coleta+</a>
+            `
+            telaMobile = true
+        }
+    }
+}
