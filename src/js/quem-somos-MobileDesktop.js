3e1258dde63215eeb95f6802e5c170776a497620
João Lucas <introducaoteste@gmail.com>
2022-11-30 13:55:34 -0300

    Mechendo na estrutura do código para centralizar a logo do site

--- src/js/quem-somos-MobileDesktop.js
+++ src/js/quem-somos-MobileDesktop.js
@@ -9,21 +9,28 @@ if (window.innerWidth >= 800) {
         </a>
     `
     document.querySelector("main section:nth-child(2)").appendChild(img)
+    telaMobile = false
 }
 
 window.onresize = () => {
-    if (window.innerWidth < 800) {
-        document.querySelector(".logo").innerHTML = `
-            <a href="../../index.html">Coleta+</a>
-        `
-        document.querySelector("main section:nth-child(2)").innerHTML = ""
-    }
     if (window.innerWidth >= 800) {
-        document.querySelector(".logo").innerHTML = `
-            <a href="../../index.html">
-                <img src="../../assets/logo-desktop.png" alt="logo" />
-            </a>
-        `
-        document.querySelector("main section:nth-child(2)").appendChild(img)
+        if (telaMobile) {
+            document.querySelector(".logo").innerHTML = `
+                <a href="../../index.html">
+                    <img src="../../assets/logo-desktop.png" alt="logo" />
+                </a>
+            `
+            document.querySelector("main section:nth-child(2)").appendChild(img)
+            telaMobile = false
+        }
+    }
+    if (window.innerWidth < 800) {
+        if (!telaMobile) {
+            document.querySelector(".logo").innerHTML = `
+                <a href="../../index.html">Coleta+</a>
+            `
+            document.querySelector("main section:nth-child(2)").innerHTML = ""
+            telaMobile = true
+        }
     }
 }
